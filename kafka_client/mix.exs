3845b51e3b29e4354bdd4881fc506944f8199c80
Saša Jurić <sasa.juric@gmail.com>
2022-08-18 13:45:04 +0200

    feat: auto compile java port

--- kafka_client/mix.exs
+++ kafka_client/mix.exs
@@ -7,7 +7,8 @@ defmodule KafkaClient.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -19,5 +20,31 @@ defmodule KafkaClient.MixProject do
 
   defp deps do
     []
+  end
+
+  defp aliases do
+    [
+      compile: [&compile_port/1, "compile"]
+    ]
+  end
+
+  defp compile_port(_args) do
+    case System.cmd(
+           "mvn",
+           ~w/clean compile assembly:single/,
+           cd: "port",
+           stderr_to_stdout: true
+         ) do
+      {_output, 0} ->
+        File.mkdir_p!("priv")
+
+        File.cp(
+          "port/target/kafka-client-1.0-SNAPSHOT-jar-with-dependencies.jar",
+          "priv/kafka-client-1.0.jar"
+        )
+
+      {output, status} ->
+        Mix.raise("#{output}\n\nPort project build exited with the status #{status}")
+    end
   end
 end
