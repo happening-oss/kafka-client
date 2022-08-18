f5cfa84b226f23a9bc409cf1ff64c7950bd9d6ea
Saša Jurić <sasa.juric@gmail.com>
2022-08-18 13:53:06 +0200

    feat: implement infinite port reading loop

--- kafka_client/lib/kafka_client/consumer.ex
+++ kafka_client/lib/kafka_client/consumer.ex
@@ -2,10 +2,15 @@ defmodule KafkaClient.Consumer do
   def start do
     Port.open(
       {:spawn_executable, System.find_executable("java")},
-      args: [
-        "-cp",
-        "#{Application.app_dir(:kafka_client)}/priv/kafka-client-1.0.jar",
-        "com.superology.KafkaConsumer"
+      [
+        :nouse_stdio,
+        :binary,
+        packet: 4,
+        args: [
+          "-cp",
+          "#{Application.app_dir(:kafka_client)}/priv/kafka-client-1.0.jar",
+          "com.superology.KafkaConsumer"
+        ]
       ]
     )
   end
