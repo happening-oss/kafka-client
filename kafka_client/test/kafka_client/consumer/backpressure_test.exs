da173831c63f9eb35f21e557cbaad3f4cb3b23fe
Saša Jurić <sasa.juric@gmail.com>
2022-09-01 15:09:58 +0200

    refactor: reduce test fragmentation
    The new organization still takes the advantage of the parallelism but the code is less fragmented.

---
+++ kafka_client/test/kafka_client/consumer/backpressure_test.exs
@@ -0,0 +1,79 @@
+defmodule KafkaClient.Consumer.BackpressureTest do
+  use ExUnit.Case, async: true
+  import KafkaClient.Test.Helper
+
+  test "buffer length based pause" do
+    consumer = start_consumer!(num_topics: 2)
+    [topic1, topic2] = consumer.topics
+
+    %{offset: last_buffered_offset} =
+      Stream.repeatedly(fn -> produce(topic1, partition: 0) end)
+      |> Stream.take(1000)
+      |> Enum.at(-1)
+
+    assert_poll(topic1, 0, last_buffered_offset)
+
+    %{offset: first_paused_offset} = produce(topic1, partition: 0)
+    refute_poll(topic1, 0, first_paused_offset)
+
+    # check that records on other topic-partitions are still consumed
+    produce(topic1, partition: 1)
+    assert_processing(topic1, 1)
+
+    produce(topic2, partition: 0)
+    assert_processing(topic2, 0)
+
+    # check that buffer is not unpaused immediately
+    Stream.repeatedly(fn -> process_next_record!(topic1, 0) end) |> Enum.take(499)
+    refute_poll(topic1, 0, first_paused_offset)
+
+    # check that topic-partition is resumed after one more record is processed
+    process_next_record!(topic1, 0)
+    assert_poll(topic1, 0, first_paused_offset)
+  end
+
+  test "messages size based pause" do
+    consumer = start_consumer!(num_topics: 2)
+    [topic1, topic2] = consumer.topics
+
+    payload = <<0::200_000-unit(8)>>
+
+    %{offset: last_buffered_offset} =
+      Stream.repeatedly(fn -> produce(topic1, partition: 0, payload: payload) end)
+      |> Stream.take(5)
+      |> Enum.at(-1)
+
+    assert_poll(topic1, 0, last_buffered_offset)
+
+    %{offset: first_paused_offset} = produce(topic1, partition: 0)
+    refute_poll(topic1, 0, first_paused_offset)
+
+    # check that records on other topic-partitions are still consumed
+    produce(topic1, partition: 1)
+    assert_processing(topic1, 1)
+
+    produce(topic2, partition: 0)
+    assert_processing(topic2, 0)
+
+    # check that buffer is not unpaused immediately
+    Stream.repeatedly(fn -> process_next_record!(topic1, 0) end) |> Enum.take(2)
+    refute_poll(topic1, 0, first_paused_offset)
+
+    # check that topic-partition is resumed after one more record is processed
+    process_next_record!(topic1, 0)
+    assert_poll(topic1, 0, first_paused_offset)
+  end
+
+  test "topic-partition is not paused if the buffer is empty" do
+    consumer = start_consumer!()
+    [topic] = consumer.topics
+
+    payload = <<0::1_000_000-unit(8)>>
+
+    %{offset: offset1} = produce(topic, partition: 0, payload: payload)
+    assert_poll(topic, 0, offset1)
+
+    %{offset: offset2} = produce(topic, partition: 0, payload: payload)
+    assert_poll(topic, 0, offset2)
+  end
+end
