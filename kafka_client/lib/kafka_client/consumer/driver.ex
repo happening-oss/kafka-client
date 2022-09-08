c008c74efa9eb6d2b6e44f974d2f921e293f3fc5
Saša Jurić <sasa.juric@gmail.com>
2022-09-08 08:16:23 +0200

    refactor: remove unused module

--- kafka_client/lib/kafka_client/consumer/driver.ex
+++
@@ -1,8 +0,0 @@
-defmodule KafkaClient.Consumer.Driver do
-  @type instance :: any
-
-  @callback open(consumer_params :: map, topics :: [String.t()], poll_interval :: pos_integer) ::
-              instance
-  @callback close(instance) :: :ok
-  @callback notify_processed(instance, topic :: String.t(), partition :: non_neg_integer) :: :ok
-end
